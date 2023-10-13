1006c84de7f5b4e3b639ab965b77b27922679202
99adarsh <itsadarshkumar@gmail.com>
2023-10-13 13:03:57 +0530

    fixed validator helper script

--- helpers/ipfs_scripts/validator_helper.js
+++ helpers/ipfs_scripts/validator_helper.js
@@ -1,6 +1,7 @@
 import { Web3Storage, getFilesFromPath } from 'web3.storage'
 import fs from 'fs';
 import path from 'path';
+import { fileURLToPath } from 'url';
 import { exec } from 'child_process'
 import axios from "axios";
 
@@ -24,7 +25,7 @@ export async function retrieve(cid) {
     }
 
     const __filename = fileURLToPath(import.meta.url);
-    const current_dir = dirname(__filename);
+    const current_dir = path.dirname(__filename);
     const downloadDirectory = path.join(current_dir, 'downloads');
 
     // Ensure the download directory exists
@@ -38,22 +39,21 @@ export async function retrieve(cid) {
 
     if (files != null) {
         for (const file of files) {
-
             const destinationPath = path.join(downloadDirectory, file.name);
             // Write the file data to the destination file
             console.log((await file.text()).length, "the length  of the file")
 
             fs.writeFileSync(destinationPath, await file.text());
             console.log("File downloaded and stored in the directory")
-
         }
-
     }
 }
 
+
 function runDockerCommand() {
+
     const dockerCommand = `
-      docker run --rm -v /home/adarsh/my_files/arufa_research/newWeb3demo/downloads/upload:/code \
+      docker run --rm -v "$(pwd)"/downloads/counter_contract:/code \
       --mount type=volume,source="$(basename "$(pwd)")_cache",target=/target \
       --mount type=volume,source=registry_cache,target=/usr/local/cargo/registry \
       cosmwasm/workspace-optimizer:0.14.0
@@ -64,7 +64,6 @@ function runDockerCommand() {
             console.error(`Error executing Docker command: ${error}`);
             return;
         }
-
         console.log(`Docker command output:\n${stdout}`);
         console.error(`Docker command errors:\n${stderr}`);
     });
@@ -79,35 +78,41 @@ async function main() {
     const verifierClient = async () => {
         await StargateClient.connect(url)
     }
+
+    // Fow checking new pending contract
     let complete_count = 0;
-    // Keep quering the contract and if any pending contract found it will compile and save in the download directory
+
+    // Keep quering the contract and if any pending contract 
+    // found it will compile and save in the download directory
     while (true) {
         // querying current pending contract id
         await axios.get(url + "/verifier/verifier/current_pending_contract").
         then(async (requestIdres) => {
-            console.log("currrent pending contract id is: ", requestIdres.data.id);
-            
+
             if (requestIdres.data.id != 0 && complete_count < requestIdres.data.id) {
-    
+                console.log("currrent pending contract id is: ", requestIdres.data.id);
+
                 // Pending contract query by it's id
                 const pendingContractres = await axios.get(url + `/verifier/verifier/contract_info/${requestIdres.data.id}`)
                 console.log("current pending contract is : ", pendingContractres.data);
-    
+
                 // const cid = "bafybeifg7wrwmd5sjobcvo4ctzkrse6adq3hfwvdq64bf2ika3ruys4inq";
-                const cid = pendingContractres.data.PendingContracts.offchainCodeUrl;
+                const cid = pendingContractres.data.contractinfo.offchainCodeUrl;
                 console.log(`CID is ${cid}`)
-    
+
                 // Download contract in contract directory from ipfs
                 await retrieve(cid);
-    
+
                 // compile the contracts downloaded in the download directory
                 console.log("starting docker compilation")
                 runDockerCommand();
                 complete_count++;
+
+                console.log("Waiting for new pending contract")
             }
+            
         })
     }
-
 }
 
 main()
