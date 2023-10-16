d4a30228f72e924cd1eb76ca24e5f711f76ce7c5
99adarsh <itsadarshkumar@gmail.com>
2023-10-16 14:39:16 +0530

    fix vote timing check

--- x/verifier/keeper/msg_server_aggregate_code_hash_prevote.go
+++ x/verifier/keeper/msg_server_aggregate_code_hash_prevote.go
@@ -11,7 +11,6 @@ import (
 func (k msgServer) AggregateCodeHashPrevote(goCtx context.Context, msg *types.MsgAggregateCodeHashPrevote) (*types.MsgAggregateCodeHashPrevoteResponse, error) {
 	ctx := sdk.UnwrapSDKContext(goCtx)
 
-	// derive validator address from tx sender address
 	valAddr, err := sdk.ValAddressFromBech32(msg.Validator)
 	if err != nil {
 		return nil, err
@@ -21,17 +20,10 @@ func (k msgServer) AggregateCodeHashPrevote(goCtx context.Context, msg *types.Ms
 	if err := k.ValidateValidator(ctx, valAddr); err != nil {
 		return nil, err
 	}
-	// Verify if not already voted
+	// Check Already voted..?
 	if k.HasAggregateCodeHashPrevote(ctx, valAddr) {
 		return nil, types.ErrExistingPrevote
 	}
-
-	//WHY....?
-	// // Convert hex string to votehash
-	// voteHash, err := types.AggregateVoteHashFromHex(msg.Hash)
-	// if err != nil {
-	// 	return nil, types.ErrInvalidHash.Wrap(err.Error())
-	// }
 
 	// Check if the prevote made by validator is in the vote period for the penidg Contarct
 	finalPrevoteTime, err := k.GetContractPrevoteTime(ctx, msg.ApplicationId)
